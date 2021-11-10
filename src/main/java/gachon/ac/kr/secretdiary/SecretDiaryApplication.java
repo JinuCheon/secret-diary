6fbc5e15884cab27bf96cc9fc947fa1579d33c50
JinuCheon <woojin8787@gmail.com>
2021-11-10 22:46:15 +0900

    first commit

---
+++ src/main/java/gachon/ac/kr/secretdiary/SecretDiaryApplication.java
@@ -0,0 +1,13 @@
+package gachon.ac.kr.secretdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gachon/ac/kr/secretdiary/SecretDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package gachon.ac.kr.secretdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
