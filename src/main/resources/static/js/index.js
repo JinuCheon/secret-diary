0dc982630ce878ead3884af0df820bd3b0a6d72c
JinuCheon <woojin8787@gmail.com>
2021-11-15 18:05:20 +0900

    feat : 이름 검색 기능
    테이블을 최초 한 번만 불러오고, 프론트에서 필터링합니다.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,8 @@
 
+let diaryList = [];
+
 (function init(){
-    getList();
+    getList(null);
 }());
 
 function formSubmit(){
@@ -9,7 +11,7 @@ function formSubmit(){
     let inputText = document.getElementById("text").value;
     console.log(inputName, inputPassword, inputText);
     axios.post('http://localhost:3000/api/NewDiary', null, {params: {name: inputName, password: inputPassword, text: inputText}})
-        .then(() => getList()); //화면 그리기 비동기 처리
+        .then(() => getList(null)); //화면 그리기 비동기 처리
 
     // axios.request({
     //     method: 'POST',
@@ -19,20 +21,23 @@ function formSubmit(){
     // }).then(() => getList());
 
 }
+function drawTable(searchValue){
+    console.log(searchValue)
+    let diaryTable = "<table><th>글번호</th><th>이름</th><th>작성시간</th>"
+    for(let i=diaryList.length - 1;i>=0;i--){
+        if(searchValue == "" || searchValue == null || searchValue == diaryList[i].name)
+            diaryTable+=`<tr><td>${diaryList[i].id}</td><td>${diaryList[i].name}</td><td>${diaryList[i].time}</td><td><button onclick="openDiary(${diaryList[i].id})">일기 열람</button></td></tr>`
+    }
+    diaryTable += "</table>"
+    document.getElementById("list").innerHTML = diaryTable;
+}
 
 function getList() {
     axios.get('http://localhost:3000/api/DiaryList')
         .then(function (response) {
-            let diaryList = response.data;
+            diaryList = response.data;
             console.log(diaryList);
-
-            let diaryTable = "<table><th>글번호</th><th>이름</th><th>작성시간</th>"
-            for(let i=diaryList.length - 1;i>=0;i--){
-                diaryTable+=`<tr><td>${diaryList[i].id}</td><td>${diaryList[i].name}</td><td>${diaryList[i].time}</td><td><button onclick="openDiary(${diaryList[i].id})">일기 열람</button></td></tr>`
-            }
-            diaryTable += "</table>"
-            console.log(diaryTable);
-            document.getElementById("list").innerHTML = diaryTable;
+            drawTable(null);
         })
         .catch(function (error) {
             console.log(error);
@@ -55,4 +60,8 @@ function openDiary(num){
         })
         .catch(function (error) {
         })
+}
+
+function searchBoxEvent(){
+    drawTable(document.getElementById("searchBox").value);
 }
