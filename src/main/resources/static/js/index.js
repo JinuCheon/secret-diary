0eb0f4e67530ad357011b4983729ada44d3f7a91
JinuCheon <woojin8787@gmail.com>
2021-11-25 17:58:03 +0900

    aws 설정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,9 +1,9 @@
 
 let diaryList = [];
-let server_url = 'http://localhost:3000';
+let server_url = 'http://3.22.3.98:80';
 
 (function init() {
-    server_url = 'http://localhost:3000';
+    server_url = 'http://3.22.3.98:80';
     getList(null);
 }());
 
